9e13d3ab6db53aa02437e22322829ca181c4c722
Ajay Joy <ajayjoy618@gmail.com>
2024-04-06 21:36:11 +0530

    mark as done button

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('addtask/', views.addTask, name='addTask')
+    path('addtask/', views.addTask, name='addTask'),
+    path('marks_as_done/<int:pk>/' , views.mark_as_done , name="mark_as_done")
 ]
