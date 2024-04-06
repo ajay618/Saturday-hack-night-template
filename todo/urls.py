bc556a2a2f4e6ce8c8419ebb0f4dbd278284ae38
Ajay Joy <ajayjoy618@gmail.com>
2024-04-06 21:44:44 +0530

    mark as undone button

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('addtask/', views.addTask, name='addTask'),
-    path('marks_as_done/<int:pk>/' , views.mark_as_done , name="mark_as_done")
+    path('marks_as_done/<int:pk>/' , views.mark_as_done , name="mark_as_done"),
+    path('marks_as_undone/<int:pk>/' , views.mark_as_undone , name="mark_as_undone"),
 ]
