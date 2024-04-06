b687d6042c2a2dec703dadd9d65e6907f2f469a2
Ajay Joy <ajayjoy618@gmail.com>
2024-04-06 21:57:15 +0530

    edit task urlpattern

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('addtask/', views.addTask, name='addTask'),
     path('marks_as_done/<int:pk>/' , views.mark_as_done , name="mark_as_done"),
     path('marks_as_undone/<int:pk>/' , views.mark_as_undone , name="mark_as_undone"),
+    path('edit_task/<int:pk>/' , views.edit_task , name="edit_task"),
 ]
